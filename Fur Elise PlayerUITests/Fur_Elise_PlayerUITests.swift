1bd520abe12a4fa0c3db9c50f98decdbe06b7a9d
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-29 16:01:13 -0400

    Initial Commit

---
+++ Fur Elise PlayerUITests/Fur_Elise_PlayerUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Fur_Elise_PlayerUITests.swift
+//  Fur Elise PlayerUITests
+//
+//  Created by Rosario Tarabocchia on 9/29/15.
+//  Copyright © 2015 RLDT. All rights reserved.
+//
+
+import XCTest
+
+class Fur_Elise_PlayerUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
