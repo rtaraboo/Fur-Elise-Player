cb506973513dcfe8c1d9fb22752ffdf8a7c2f177
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-29 16:53:25 -0400

    Initial Commit

--- Fur Elise Player/ViewController.swift
+++ Fur Elise Player/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player : AVAudioPlayer = AVAudioPlayer()
 
+    @IBOutlet weak var sldScrub: UISlider!
+    @IBOutlet weak var sldVolume: UISlider!
+    
+    
+    func updateSldScrub(){
+    
+    sldScrub.value = Float(player.currentTime)
+    
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        do {
+        
+        
+        try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("fur_elise", ofType: "mp3")!))
+            
+            sldScrub.maximumValue = Float(player.duration)
+            sldScrub.minimumValue = 0.0
+            
+        } catch {
+        
+        // Didnt work
+        
+        }
+        
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateSldScrub"), userInfo: nil, repeats: true)
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +60,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnPlayACTION(sender: AnyObject) {
+        
+        player.play()
+    }
 
+    @IBAction func sldVolumeACTION(sender: AnyObject) {
+        
+        player.volume = sldVolume.value
+    }
+    
+    @IBAction func sldScrubACTION(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(sldScrub.value)
+        
+    }
+    
+    
+    @IBAction func btnStop(sender: AnyObject) {
+        
+        player.pause()
+        
+        
+        do {
+            
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("fur_elise", ofType: "mp3")!))
+            
+        } catch {
+            
+            // Didnt work
+            
+        }
+        
+    }
+    
+    
+    @IBAction func btnPause(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
